576c4c83017ec346d73d2296152a9141060939ef
Anastasiia Tabachnikova <nastiatab11@gmail.com>
2025-07-11 15:34:18 +0200

    Add new files and changes to existing files for fetching data with react query

--- src/app/components/status-label.tsx
+++ src/app/components/status-label.tsx
@@ -1,37 +1,22 @@
 import React from 'react';
-import clsx from 'clsx';
+import { Company, getCompany } from '@/lib/api';
+import getQueryClient from '@/lib/utils/getQueryClient';
+import Header from '@/app/components/header';
 
-export enum Status {
-  Active = 'active',
-  NotActive = 'notActive',
-  Pending = 'pending',
-  Suspended = 'suspended',
+export interface PageProps {
+  params: { id: string };
 }
 
-export interface StatusLabelProps {
-  children: React.ReactNode;
-  status: Status;
-  disabled?: boolean;
-}
+export default async function Page({ params }: PageProps) {
+  const queryClient = getQueryClient();
 
-function StatusLabel({ children, status, disabled }: StatusLabelProps) {
-  return (
-    <div
-      className={clsx(
-        'inline-flex items-center py-1 px-3.5 rounded-3xl text-sm font-medium',
-        status === Status.Active && 'text-green-700 bg-green-100',
-        status === Status.NotActive && 'text-red-700 bg-red-100',
-        status === Status.Pending && 'text-orange-700 bg-orange-100',
-        status === Status.Suspended && 'text-blue-700 bg-blue-100',
-        {
-          ['opacity-75 cursor-not-allowed']: disabled,
-        },
-      )}
-    >
-      <div className="w-1 h-1 mr-2 rounded-full bg-current" />
-      {children}
-    </div>
-  );
-}
+  await queryClient.prefetchQuery({
+    queryKey: ['companies', params.id],
+    queryFn: () => getCompany(params.id, { cache: 'no-store' }),
+    staleTime: 10 * 1000,
+  });
 
-export default StatusLabel;
+  const company = queryClient.getQueryData(['companies', params.id]) as Company;
+
+  return <Header>{company?.title}</Header>;
+}
